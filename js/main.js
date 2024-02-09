df0a64273e280c654ed302575e5be6f2b0575887
E.V.Komarov <OneDarker@yandex.ru>
2024-02-09 10:30:51 +0700

    test penModule

--- js/main.js
+++ js/main.js
@@ -230,7 +230,7 @@ const submitButton = document.getElementById('submitButton');
 const outputElement = document.getElementById('output');
 
 //При нажатии кнопки "submitButton"
-submitButton.addEventListener('click', function() { 
+/*submitButton.addEventListener('click', function() { 
 	const editTextValue = editTextElement.value;
 	// Пример использования
 	const inputText = "(([trunc with digits (4)] of (4.126795)))";
@@ -244,13 +244,76 @@ submitButton.addEventListener('click', function() {
 	
 	
 	myFunction(`Текст без скобок: ${result.text}\nЗначения в скобках: ${result.args.join(', ')}`);
-});
+});*/
 
 //Вывод значения в "output"
 function myFunction(text) {
     // Ваш код, использующий текст из editText
     outputElement.innerHTML = 'Received text: ' + text;
 }
+
+/*
+Модуль penModule
+
+
+*/
+class CanvasModule {
+  constructor(canvasId) {
+    this.canvas = document.getElementById(canvasId);
+    this.ctx = this.canvas.getContext('2d');
+    this.canvasWidth = window.innerWidth;
+    this.canvasHeight = window.innerHeight;
+    this.targetFPS = 60;
+    this.deltaTime = 1 / this.targetFPS;
+    this.lastTime = 0;
+
+    this.initCanvas();
+    this.animate();
+  }
+
+  initCanvas() {
+    this.canvas.width = this.canvasWidth;
+    this.canvas.height = this.canvasHeight;
+    window.addEventListener('resize', () => this.adaptCanvasSize());
+  }
+
+  adaptCanvasSize() {
+    this.canvasWidth = window.innerWidth;
+    this.canvasHeight = window.innerHeight;
+    this.canvas.width = this.canvasWidth;
+    this.canvas.height = this.canvasHeight;
+  }
+
+  clearCanvas() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+
+  drawSquare(squareX, squareY, squareSize, rotationAngle, color) {
+    this.ctx.save();
+    this.ctx.translate(squareX + squareSize / 2, squareY + squareSize / 2);
+    this.ctx.rotate((Math.PI / 180) * rotationAngle);
+    this.ctx.fillStyle = color;
+    this.ctx.fillRect(-squareSize / 2, -squareSize / 2, squareSize, squareSize);
+    this.ctx.restore();
+  }
+
+  animate(currentTime) {
+    requestAnimationFrame((time) => this.animate(time));
+
+    const deltaTime = (currentTime - this.lastTime) / 1000;
+    if (deltaTime < 1 / this.targetFPS) return;
+
+    this.clearCanvas();
+    // Ваши дополнительные рисования или обновления сцены здесь
+
+    this.lastTime = currentTime;
+  }
+}
+
+// Пример использования
+const canvasModule = new CanvasModule('adaptiveCanvas');
+
+
 
 const inputText = "(9)+((1)+(1))";
 const result = openBrackets(inputText);
