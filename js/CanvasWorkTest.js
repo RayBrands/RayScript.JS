fa96413dde340d6f9106a3873fe12f495ff3a45b
E.V.Komarov <OneDarker@yandex.ru>
2024-02-11 07:15:55 +0700

    Canvas Work

--- js/CanvasWorkTest.js
+++ js/CanvasWorkTest.js
@@ -1,35 +1,45 @@
-const canvas = document.getElementById('adaptiveCanvas');
+const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+const canvasContainer = document.getElementById("canvas-container");
 
 // Функция для определения FPS
-let fps = 10;
+let fps = 60;
 let then = Date.now();
 
-let player = {
-	  x: 0,
-	  y: 0
+//"Сцена" и её стандартные свойства
+let stage = {
+	height: 700,
+	width: 200,
+	scaleWidth: 1,
+	scaleHeight: 1,
+  scaleMode: 'scaled' //Определение "типа" отрисовки
 };
-	
+
+//Тестовые значения
+let player = {
+  x: 0,
+  y: 0
+};
+//Обновление экрана 
+//TODO: Сделать оптимизацию по поводу FPS (Желательно без использования timeout)
 function update() {
 	
+  //Определение DeltaTime
   const now = Date.now();
   const dt = (now - then) / 1000;
   then = now;
 
-  // Игровая логика
-	
-
-
-
   // Очистка холста
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-	  // Обновление игрового мира
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, stage.width, stage.height);
+ 
+  //Тест передвижения игрока на +1 пиксель
   player.x += 1;
-
   // Отрисовка игрового мира
   ctx.fillStyle = 'red';
-  ctx.rotate((Math.PI / 180) * 10);
+  //ctx.rotate((Math.PI / 180) * 10);
   ctx.fillRect(player.x, player.y, 50, 50);
+  
   // Обновление игрового мира (логика игры)
 
   // Отрисовка игрового мира (графика)
@@ -38,38 +48,65 @@ function update() {
   setTimeout(update, 1000 / fps);
 }
 
-// Функция для изменения размера canvas
-let mode = 'fixed'; // 'fixed', 'scaled', 'adaptive'
-//"Сцена"
-let stage = {
-	height: 100,
-	width: 100,
-	scaleWidth: 1,
-	scaleHeight: 1
-};
-
+// Функция для изменения размера canvas в зависимости от типа прорисовки
 function resizeCanvas() {
-	switch (mode) {
-		case 'fixed':
+  //Сохранение буфера кадра перед изменением размера экрана (Чтобы не было фликов во время изменений размера окна)
+  var tempCanvas = document.createElement('canvas');
+  tempCanvas.width = canvas.width;
+  tempCanvas.height = canvas.height;
+  var tempContext = tempCanvas.getContext("2d");
+  tempContext.drawImage(ctx.canvas, 0, 0);
+  //Какие действия нужно сделать с основным Canvas
+  //canvas.style.transform = `scale(1,2)`;
+  //let scaleVar = '2';
+  //canvas.style.transform = `scale(${scaleVar},2)`;
+  canvas.width = stage.width;
+  canvas.height = stage.height;
+  //Нарисовать в новом canvas изображение старого кадра
+  ctx.drawImage(tempContext.canvas, 0, 0);
+  switch (stage.scaleMode) {
+    case 'fixed': //Фиксированый размер Сцены, располагается по центру, имеет соотношение 1:1
+      canvas.style.transform = "scale(1)";
+      break;
+    case 'scaled': //Фиксированный размер Сцены, растягивется на весь экран соотношение зависит от размера экрана
+      //TODO: Поработать над этим, либо же удалить нахуй
+      stage.scaleWidth = window.innerWidth/stage.width
+      stage.scaleHeight = window.innerHeight/stage.height
+      if (stage.scaleWidth < stage.scaleHeight) {
+				canvas.style.transform = `scale(1,${stage.scaleHeight})`;
+			} else {
+				canvas.style.transform = `scale(${stage.scaleWidth},1)`;
+			}
+      break;
+    case 'adaptive':
+      stage.width = window.innerWidth;
+      stage.height = window.innerHeight;
+      canvas.style.transform = `scale(1)`;
+      break;
+    case 'adaptive+scale': //Сделать масшабирование, в зависимости от коэффециента
+      break;
+  };
+  canvas.width = stage.width;
+  canvas.height = stage.height;
+	//switch (mode) {
+		/*case 'fixed':
 			stage.scaleWidth = window.innerWidth/stage.width
 			stage.scaleHeight = window.innerHeight/stage.height
+      canvas.width = stage.width;
+      canvas.height = stage.height;
 			
 			if (stage.scaleWidth < stage.scaleHeight) {
-				canvas.width = stage.width*stage.scaleWidth;
-				canvas.height = stage.height*stage.scaleWidth;
-				ctx.scale(stage.scaleWidth,stage.scaleWidth);
+				canvas.style.transform = 'scale(${stage.scaleWidth},${stage.scaleWidth})';
 			} else {
-				canvas.width = stage.width*stage.scaleHeight;
-				canvas.height = stage.height*stage.scaleHeight;
-				ctx.scale(stage.scaleHeight,stage.scaleHeight);
+				canvas.style.transform = 'scale(${stage.scaleHeight},${stage.scaleHeight})';
 			}
+      canvas.style.transform = 'scale(${stage.scaleHeight},${stage.scaleHeight})';
 		  // Определённый размер
-		  let dpr = window.devicePixelRatio;
-		  ctx.imageSmoothingEnabled = false;
-		  ctx.setTransform(stage.scaleHeight, 0, 0, stage.scaleHeight, 0, 0);
+		  //ctx.setTransform(stage.scaleHeight, 0, 0, stage.scaleHeight, 0, 0);
 		  //window.innerWidth / canvas.width, window.innerHeight / canvas.height);
-		  canvas.style.top = (window.innerHeight - canvas.height) / 2 + 'px';
-		  canvas.style.left = (window.innerWidth - canvas.width) / 2 + 'px';
+		  //canvas.style.top = (window.innerHeight - canvas.height) / 2 + 'px';
+		  //canvas.style.left = (window.innerWidth - canvas.width) / 2 + 'px';
+      
 		  break;
 		case 'scaled':
 		  // Масштабирование
@@ -82,26 +119,29 @@ function resizeCanvas() {
 		  canvas.width = window.innerWidth;
 		  canvas.height = window.innerHeight;
 		  break;
-	  }
+	  }*/
+    //canvas.width = stage.width;
+    //canvas.height = stage.height;
+    //const scale = Math.min(canvasContainer.clientWidth / canvas.width, canvasContainer.clientHeight / canvas.height);
+		//canvas.style.transform = `scale(${scale})`;
 }
-
 
 // Функция для изменения режима отображения
 function changeDisplayMode() {
-  switch (mode) {
+  switch (stage.scaleMode) {
     case 'fixed':
       // Определённый размер
-      mode = 'fixed';
+      stage.scaleMode = 'fixed';
       resizeCanvas();
       break;
     case 'scaled':
       // Масштабирование
-      mode = 'scaled';
+      stage.scaleMode = 'scaled';
       resizeCanvas();
       break;
     case 'adaptive':
       // Адаптивный размер
-      mode = 'adaptive';
+      stage.scaleMode = 'adaptive';
       resizeCanvas();
       break;
   }
@@ -109,8 +149,9 @@ function changeDisplayMode() {
 
 
 // Запуск игры
+ctx.imageSmoothingEnabled = true;
 resizeCanvas();
-changeDisplayMode();
+//changeDisplayMode();
 update();
 
 // Обработчики событий
@@ -119,15 +160,15 @@ window.addEventListener('keydown', (event) => {
   // Обработка нажатия клавиш
   switch (event.keyCode) {
     case 38: // Up arrow
-      mode = 'fixed';
+      stage.scaleMode = 'fixed';
       changeDisplayMode();
       break;
     case 40: // Down arrow
-      mode = 'scaled';
+      stage.scaleMode = 'scaled';
       changeDisplayMode();
       break;
     case 39: // Right arrow
-      mode = 'adaptive';
+      stage.scaleMode = 'adaptive';
       changeDisplayMode();
       break;
   }
